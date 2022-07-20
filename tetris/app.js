54c7cfba5b33e63dbb16c6b63cfc5e09376bc7d6
Olivia jingyao <oliviachen797@gmail.com>
2022-07-20 22:00:08 +1000

    half done

---
+++ tetris/app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+        //Learn forEach and arrow function:
+        // let names = ["张飞", "Olivia", "曹操", "刘备"];
+        // names.forEach((xingcan) => {
+        //     console.log(xingcan + ", is the best!");
+        // })
+
+    }) //当纯 HTML 被完全加载以及解析时，DOMContentLoaded 事件会被触发，而不必等待样式表，图片或者子框架完成加载。
